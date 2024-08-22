531f9703205282fc2cae7a302e3e1f3051c8631a
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-08-22 20:39:18 +0330

    Add cqrs abstractions

---
+++ Library.WebAPI/Abstractions/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Library.WebAPI.Abstractions;
+
+public interface ICommand : ICommand<Unit>;
+
+public interface ICommand<out TResponse>
+    : IRequest<TResponse>
+    where TResponse : notnull;
---
+++ Library.WebAPI/Abstractions/IQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Library.WebAPI.Abstractions;
+
+public interface IQuery<out TResponse> 
+    : IRequest<TResponse>
+    where TResponse : notnull;
